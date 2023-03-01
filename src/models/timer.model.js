0100797e9b792951733ee68b4486c70db9972ab0
Neal A <73256760+nealarch01@users.noreply.github.com>
2023-03-01 11:07:06 -0800

    Remove model methods
    Electron doesn't include functions

--- src/models/timer.model.js
+++ src/models/timer.model.js
@@ -13,50 +13,6 @@ class Timer {
 
         this.interval = null;
     }
-
-    tick() {
-        if (this.timeLeft <= 0) {
-            this.timeLeft = 0;
-            return;
-        }
-        this.timeLeft--;
-        console.log(timeLeft);
-        return this.displayTime();
-    }
-
-    start() {
-        this.isRunning = true;
-        this.interval = setInterval(this.tick, 1000);
-    }
-
-    stop() {
-        this.isRunning = false;
-        clearInterval(this.interval); 
-    }
-
-    resetRoundTime() {
-        this.timeLeft = this.roundDuration;
-    }
-
-    resetBreakTime() {
-        this.timeLeft = this.breakDuration;
-    }
-
-    displayTime() {
-        // Convert seconds to minutes and seconds
-        let minutes = Math.floor(this.timeLeft / 60);
-        let seconds = this.timeLeft % 60;
-        return `${minutes}:${seconds}`
-    }
-
-    startRound() {
-        this.timeLeft = this.roundDuration;
-    }
-
-    startBreak() {
-        this.timeLeft = this.breakDuration;
-    }
-
 }
 
 module.exports = {
